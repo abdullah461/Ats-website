ce507b0bb36d1342c5eb2dcfdcdecfeeefd08b70
abdull461 <abdullahabubakar461@gmail.com>
2023-08-22 08:12:01 +0100

    added more styles

--- index.js
+++ index.js
@@ -43,3 +43,18 @@
     }());
 
 
+// hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector("ul");
+const links = document.querySelectorAll("li");
+
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
